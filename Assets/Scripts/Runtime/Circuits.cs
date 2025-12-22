f6de904c6f6de26f76f6db12b8f943ab5cafe9a2
cryxan <john.sinclair@daoloth.me.uk>
2025-12-22 23:26:41 +0000

    Initial pass on day 8

---
+++ Assets/Scripts/Runtime/Circuits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace AdventOfCode
+{
+    public class Circuits
+    {
+        List<Vector3Int> circuits = new List<Vector3Int>();
+        
+        public Circuits()
+        {
+            
+        }
+
+        public int LoadAndAnalyseCircuits(string filename)
+        {
+            if (!File.Exists(filename)) return -1;
+            
+            var text = File.ReadAllText(filename);
+
+            var circuitLocationStrings = text.Split("\r\n");
+            foreach (var circuitLocationString in circuitLocationStrings)
+            {
+                var locationArray = circuitLocationString.Split(',');
+                var location = new Vector3Int(
+                    int.Parse(locationArray[0]),
+                    int.Parse(locationArray[1]),
+                    int.Parse(locationArray[2]));
+                circuits.Add(location);
+            }
+
+            return -2;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Tests/CircuitsTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using AdventOfCode;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode
+{
+    public class CircuitsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TestCase("Day8CircuitExample.txt", 40)]
+        [TestCase("Day8Circuit.txt", 0)]
+        void CircuitsReturnExpectedValue(string filename, int expected)
+        {
+            var fileToLoad = Path.Combine(GetThisFilePath(), "TestFiles", filename);
+            
+            
+        }
+        
+        private static string GetThisFilePath([CallerFilePath] string path = null)
+        {
+            return Path.GetDirectoryName(path);
+            
+        }
+    }
+}
