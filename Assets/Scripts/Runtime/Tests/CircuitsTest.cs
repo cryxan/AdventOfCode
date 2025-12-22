6c780836da0c703bd6250c0a6f72278593be1f6b
cryxan <john.sinclair@daoloth.me.uk>
2025-12-22 23:32:12 +0000

    WIP

--- Assets/Scripts/Runtime/Tests/CircuitsTest.cs
+++ Assets/Scripts/Runtime/Tests/CircuitsTest.cs
@@ -12,10 +12,12 @@ namespace AdventOfCode
 {
     public class CircuitsTest
     {
+        Circuits  circuits;
+        
         [SetUp]
         public void Setup()
         {
-            
+            circuits = new Circuits();
         }
 
         [TestCase("Day8CircuitExample.txt", 40)]
@@ -24,7 +26,9 @@ namespace AdventOfCode
         {
             var fileToLoad = Path.Combine(GetThisFilePath(), "TestFiles", filename);
             
+            var result = circuits.LoadAndAnalyseCircuits(fileToLoad);
             
+            Assert.That(result, Is.EqualTo(expected));
         }
         
         private static string GetThisFilePath([CallerFilePath] string path = null)
